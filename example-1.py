00bf076959be92d166d706473970e3bae2efb937
bcarnage <92232444+bcarnage@users.noreply.github.com>
2024-04-12 18:25:04 +0100

    adding functions to document

---
+++ example-1.py
@@ -0,0 +1,18 @@
+def function1(arg1, arg2):
+    """
+    this function takes two arguments, sets the first to equel the second then returnes the first argument. pointless
+
+    :param arg1: some value
+    :param arg2: anouther value
+
+    :return: arg1
+    """
+    arg1 = arg2
+    return arg1
+def function2():
+    """
+    pointless function with no arguents returnes none
+
+    :return: None
+    """
+    return None
